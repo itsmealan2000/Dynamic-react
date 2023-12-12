5aff3a7727323dcac1c645a4914e0f2d1675f1ce
Alan v John <itsmealan2000@gmail.com>
2023-12-12 15:59:01 +0530

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import Navbar from './Components/Navbar/Navbar.jsx'
+import Navbar from './Components/Navbar.jsx'
 function App() {
   return (
         <>
