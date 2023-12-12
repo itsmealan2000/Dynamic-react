14778137e761d93f29f5130ef8c7335be65ef364
Alan v John <itsmealan2000@gmail.com>
2023-12-12 16:20:19 +0530

    removed home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar.jsx'
-import Home from './Pages/Home.jsx'
+
 function App() {
   return (
         <>
           <Navbar />
-          <Home />
+
         </>
   )}
 export default App
